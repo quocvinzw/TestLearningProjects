aa352afd5d8ac72206645fe07b589b2a407e4a8f
QuocVinz <ngtrqvinh1810@gmail.com>
2024-04-06 01:25:09 +0700

    Add project files.

---
+++ TestLearningProjects/Controllers/PassingListJSToController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestLearningProjects.Models;
+
+namespace TestLearningProjects.Controllers
+{
+    public class PassingListJSToController : Controller
+    {
+        public ActionResult SenddingList()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void ReceiveList(List<PassingList> rcList)
+        {
+            //Save Into Database Or Do Smth Else 
+            rcList.ForEach(rc =>
+            {
+                Console.WriteLine(rc);
+            });
+        }
+    }
+}
---
+++ TestLearningProjects/Models/PassingList.cs
@@ -0,0 +1,9 @@
+﻿namespace TestLearningProjects.Models
+{
+    public class PassingList
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string amount { get; set; }
+    }
+}
